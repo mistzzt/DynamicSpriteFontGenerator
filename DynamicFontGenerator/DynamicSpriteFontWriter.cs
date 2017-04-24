fcc9eaed46e851934031c82a14d1559a23db33ee
ZIT WANG <mymisting@gmail.com>
2017-04-24 18:55:39 +0800

    完成初步字体生成

---
+++ DynamicFontGenerator/DynamicSpriteFontWriter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+using ReLogic.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace DynamicFontGenerator
+{
+	[ContentTypeWriter]
+	public class DynamicSpriteFontWriter : ContentTypeWriter<DynamicSpriteFont>
+	{
+		private const int LineSpacing = 21;
+
+		private const float CharacterSpacing = 0;
+
+		private const char DefaultCharacter = '*';
+
+		public override string GetRuntimeReader(TargetPlatform targetPlatform)
+		{
+			return "ReLogic.Graphics.DynamicSpriteFontReader, ReLogic, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+		}
+
+		protected override void Write(ContentWriter output, DynamicSpriteFont value)
+		{
+			output.Write(CharacterSpacing);
+			output.Write(LineSpacing);
+			output.Write(DefaultCharacter);
+
+			output.Write(Generator._pages.Length);
+
+			foreach (var page in Generator._pages)
+			{
+				output.WriteObject(page.Item1);
+				output.WriteObject(page.Item2.Glyphs);
+				output.WriteObject(page.Item2.Padding);
+				output.WriteObject(page.Item2.Characters);
+				output.WriteObject(page.Item2.Kerning);
+			}
+		}
+	}
+}
---
+++ DynamicFontGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DynamicFontGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DynamicFontGenerator")]
+[assembly: AssemblyCopyright("Copyright © MistZZT 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df94e648-fe2b-4833-8e42-83f73befee84")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
